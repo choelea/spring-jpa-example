ffb75c73502dd3b9360aad927b7a3eefa48335d1
Joe Li <joe.lea@foxmail.com>
2018-04-09 23:18:42 +0800

    initiate

---
+++ src/main/java/com/joe/springjpaexample/SpringJpaExampleApplication.java
@@ -0,0 +1,12 @@
+package com.joe.springjpaexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaExampleApplication.class, args);
+	}
+}
