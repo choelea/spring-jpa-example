e7e6872eb17d663898db4478a329f81b87456b77
Joe Li <joe.lea@foxmail.com>
2022-03-31 21:54:54 +0800

    set batch size to 10000

--- src/main/java/com/joe/springjpaexample/service/ManagementService.java
+++ src/main/java/com/joe/springjpaexample/service/ManagementService.java
@@ -6,10 +6,9 @@ import com.joe.springjpaexample.repo.CategoryRepository;
 import com.joe.springjpaexample.repo.ProductRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomUtils;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -23,14 +22,13 @@ import java.util.List;
 public class ManagementService {
     private ProductRepository pRepo;
     private CategoryRepository cateRepo;
-
-    public ManagementService(ProductRepository pRepo, CategoryRepository cateRepo) {
+    private ThreadPoolTaskExecutor taskExecutor;
+    public ManagementService(ProductRepository pRepo, CategoryRepository cateRepo, ThreadPoolTaskExecutor taskExecutor) {
         this.pRepo = pRepo;
         this.cateRepo = cateRepo;
+        this.taskExecutor = taskExecutor;
     }
 
-
-    @Async
     public void initData() {
         log.info("################################### Started Initiating Data ##################################");
         createCategories();
@@ -64,8 +62,9 @@ public class ManagementService {
      * For many to many relations, needs to be aware of 'bidirectional consistency problem'. Learn more from below link:
      * https://stackoverflow.com/questions/13370221/jpa-hibernate-detached-entity-passed-to-persist
      */
-    @Transactional
+//    @Transactional
     public void addProduct() {
+
         Category cate1 = cateRepo.getByCode("cate-1");
         Category cate2 = cateRepo.getByCode("cate-2");
         Category cate3 = cateRepo.getByCode("cate-3");
@@ -77,11 +76,21 @@ public class ManagementService {
         categories.add(cate4);
 
         int batchSize = 10000;
-        for (int i = 0; i < 80; i++) {
-            Category c1 = categories.get(RandomUtils.nextInt(0, 4));
-            Category c2 = categories.get(RandomUtils.nextInt(0, 4));
-            insertProducts(i*batchSize, batchSize, c1, c2);
+        for (int i=0; i < 80; i++) {
+            int finalI = i;
+            taskExecutor.execute(()->{
+                Category c1 = categories.get(RandomUtils.nextInt(0, 4));
+                Category c2 = categories.get(RandomUtils.nextInt(0, 4));
+                insertProducts(finalI *batchSize, batchSize, c1, c2);
+            });
+
         }
+
+
+        log.info(""+taskExecutor.getActiveCount());
+        log.info(""+taskExecutor.getCorePoolSize());
+        log.info(""+taskExecutor.getMaxPoolSize());
+        log.info(""+taskExecutor.getPoolSize());
     }
 
 
@@ -99,6 +108,6 @@ public class ManagementService {
             product.getCategories().add(cate2);
             list.add(product);
         }
-        pRepo.saveAll(list);
+        pRepo.saveAll(list);//经测试， 效率要高很多
     }
 }
